c49af9e1ee2844deef56b26ebcd7adbc8a017470
pupster90 <m3elliot@eng.ucsd.edu>
2018-07-29 07:01:50 +0000

    started working on network visualizer

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/judoscape:/root/Documents --name judoscape pupster90/judoscape:2
+docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/judoscape:/root/Documents --name cytoscape pupster90/cytoscape:2_3
+
