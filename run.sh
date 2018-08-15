c6a2bde86aaf3de5448dba395634e5873189e9c3
pupster90 <m3elliot@eng.ucsd.edu>
2018-08-15 02:44:13 +0000

    Safety Save: visualize running slow

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/judoscape:/root/Documents --name cytoscape pupster90/cytoscape:2_3
+docker run -it -p 80:8888 -v /home/elliott_matthew_a_t/judoscape:/root/Documents --name cytoscape pupster90/cytoscape:2_5
 
